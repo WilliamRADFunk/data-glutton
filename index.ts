e4f3839cc3a5acce8f26bd49dc1942d9c5fa049a
William Funk <wfunk@modusoperandi.com>
2019-04-16 22:13:36 -0400

    Lets factbook do the scrape loop.

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import * as fs from 'graceful-fs';
 import * as path from 'path';
 
-import { getCountries, store, getCountryURL, getCountryData, flushStore, getImages, saveFiles } from 'factbook';
+import { getCountries, getCountriesData, store } from 'factbook';
 
 if (!fs.existsSync(`dist`)) {
     fs.mkdirSync('dist');
@@ -13,42 +13,9 @@ if (!fs.existsSync(`dist`)) {
     fs.mkdirSync(path.join('dist', 'images'));
 }
 
-const getCountriesData = () => {
-    const countryDataPromises: Array<Promise<any>> = [];
-    const countries = store.countriesInList.slice();
-    countries.forEach(country => {
-        const url = getCountryURL(country.isoCode);
-        countryDataPromises.push(getCountryData(country, url));
-    });
-    return countryDataPromises;
-};
-
-const promisesResolutionForCountries = () => {
-    store.countriesInList.sort();
-    const promises = getCountriesData();
-    Promise.all(promises)
-        .then(() => {
-            if (store.failedCountries.length) {
-                store.countriesInList = store.failedCountries.slice();
-                store.failedCountries.length = 0;
-                setTimeout(() => {
-                    store.debugLogger('Waiting 3 seconds before retrieving missed countries...');
-                    promisesResolutionForCountries()
-                }, 3000);
-            } else {
-                saveFiles();
-                getImages();
-                flushStore();
-            }
-        })
-        .catch(err => {
-            store.errorLogger(new Date().toISOString() + '\n\n' + err.toString() + '\n\n');
-        });
-}
-
 Promise.all([getCountries()])
     .then(() => {
-        promisesResolutionForCountries();
+        getCountriesData();
     })
     .catch(err => {
         store.errorLogger(new Date().toISOString() + '\n\n' + err.toString() + '\n\n');
