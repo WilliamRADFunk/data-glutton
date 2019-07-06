d6a3dbf6f1ccd081a1eff0d2abe2786fc1b2a2ce
William RAD Funk <contact@WILLIAMRADFunk.com>
2019-07-05 19:17:44 -0700

    Better separates front from back.

--- data-glutton-ui/src/app/app.component.ts
+++ data-glutton-ui/src/app/app.component.ts
@@ -1,18 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
-import { store } from './constants/globalStore';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   collapsed: boolean = true;
-
-  async ngOnInit(): Promise<void> {
-    await store.createDB();
-  }
 
   toggleNavbar() {
     this.collapsed = !this.collapsed;
